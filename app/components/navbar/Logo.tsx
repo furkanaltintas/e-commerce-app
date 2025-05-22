30874dd4765bbe5759748fa4c7bf0ab28d8fe2da
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-05-22 14:39:05 +0300

    Logo tarafı yapıldı

--- app/components/navbar/Logo.tsx
+++ app/components/navbar/Logo.tsx
@@ -1,8 +1,13 @@
+import Link from "next/link";
 
 const Logo = () => {
-  return (
-    <div className="bg-orange-700 px-2 py-1 rounded-md text-lg md:text-2xl cursor-pointer">Burada <span className="text-sm">.com</span></div>
-  )
-}
+  return ( 
+    <div className="bg-orange-700 px-2 py-1 rounded-md text-lg md:text-2xl cursor-pointer hover:bg-orange-800 transition">
+      <Link href="/">
+        Burada <span className="text-sm">.com</span>
+      </Link>
+    </div>
+  );
+};
 
-export default Logo
+export default Logo;

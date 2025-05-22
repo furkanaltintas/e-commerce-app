10f696326c02ff7007054f150732cf9aae2d7f12
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-05-22 14:37:43 +0300

    Footer tarafı yapıldı

--- app/components/footer/Footer.tsx
+++ app/components/footer/Footer.tsx
@@ -1,7 +1,10 @@
+import Link from "next/link"
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className="w-full h-24 bg-orange-600 text-slate-100 flex justify-center items-center mt-3 md:mt-10">
+      <Link href={"/"} className="hover:text-slate-800 transition">Burada.com</Link>
+    </div>
   )
 }
 
